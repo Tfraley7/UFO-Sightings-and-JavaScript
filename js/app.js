fb856f7a960c1a09bf790e6d56eec4218a70a858
tfraley7 <tyfraley81@gmail.com>
2022-11-07 17:30:29 -0500

    Updating JavaScript Work

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// import the data from data.js
+const tableData = data;
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+// Simple JavaScript console.log statement
+function printHello() {
+    console.log("Hello there!");
+  }
+  // Takes two numbers and adds them
+  function addition(a, b) {
+    return a+b;
+  }
+  // Functions can call other functions
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  }
+  // Simple JavaScript log statement
+function printHello() {
+    return "Hello there!";
+  }
+  printHello = () => "Hello there!";
+  // Original addition function
+function addition(a, b) {
+    return a + b;
+  }
+  // Converted to an arrow function
+addition = (a, b) => a + b;
+// Original doubleAddition function
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  }
+  doubleAddition = (c, d) => addition(c, d) * 2;
+  
